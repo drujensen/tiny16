a3b2dc75377cb3228fdbfacc10988f28b8b24072
Dru Jensen <drujensen@gmail.com>
2023-11-17 17:24:09 -0800

    feat: new counter

---
+++ examples/up_and_down.asm
@@ -0,0 +1,19 @@
+// count up to 255 and output the result
+// then count down to 0 and output the result
+
+1A00  LD x2, #0
+
+up:
+0220  OUT x2
+3A01  ADD x2, #1
+EAFF  CMP x2, #255
+F802  BREQ :down
+C001  JMP :up
+down:
+0220  OUT x2
+4A01  SUB x2, #1
+EA00  CMP x2, #0
+F802  BREQ #0, :loop
+C006  JMP :down
+loop:
+C001  JMP :up
