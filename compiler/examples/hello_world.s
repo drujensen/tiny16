5997f9ad824b4e1cbc4f452a4d6f796b0e655fd9
Dru Jensen <drujensen@gmail.com>
2023-11-30 17:56:03 -0800

    fix: use zero

--- compiler/examples/hello_world.s
+++ compiler/examples/hello_world.s
@@ -5,14 +5,14 @@ next:  LDP a2, a1	        ; load indirect value at A1 to A2
 			 OUT a2             ; output A
 			 JSR :delay         ; delay
 			 ADD a1, s0         ; increment A1
-			 BNE a2, x0, :next  ; branch to init if equal
+			 BNE a2, zero, :next  ; branch to init if equal
        JMP :init
 
 delay: LLI a3, :timer
 			 LDP a3, a3
 loop:  SUB a3, s0
 			 NOP
-			 BNE a3, x0, :loop
+			 BNE a3, zero, :loop
 			 RET
 
 timer: 0xFFFF
